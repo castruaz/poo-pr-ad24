4b4d4a5a84748baec508309a6a7a30dae8f96f02
castruaz <castr@uaz.edu.mx>
2024-09-01 16:45:57 -0600

    Practica 3

--- p51_SerieFibonaci.java
+++ p51_SerieFibonaci.java
@@ -2,7 +2,7 @@
  * p51_SerieFibonaci - Imprime n terminos de la serie de fibonacci
  */
 
- import java.util.Scanner;
+import java.util.Scanner;
 
 public class p51_SerieFibonaci {
     public static void main(String[] args) {
@@ -10,7 +10,6 @@ public class p51_SerieFibonaci {
         boolean repetir = true;
 
         while(repetir) {
-
             System.out.print("\033[H\033[2J");System.out.flush();
             System.out.print("Ingrese el número de términos que desea imprimir: ");
             int n = scanner.nextInt();
@@ -21,7 +20,6 @@ public class p51_SerieFibonaci {
             System.out.println("Serie de Fibonacci hasta " + n + " términos:");
             for (int i = 1; i <= n; i++) {
                 System.out.print(a + " ");
-
                 // Calcula el siguiente término
                 int temp = a;
                 a = b;
